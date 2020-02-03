bb6a2fe7e1c816e9f1ce063ef607974e16019e5b
이수진 <jmakeaz@gmail.com>
2020-02-03 22:42:35 +0900

    Add router and apollo-boost

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Apollo 2020
 
 Movie app built with React, Apollo and GraphQL.
+
+- HashRouter, BrowserRouter
+브라우저 라우터는 동적요청을 처리하는 서버가 존재할 경우 사용하고,
+해시라우터는 정적 웹 사이트에 사용된다.
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { ApolloProvider } from "@apollo/react-hooks";
+import client from "./client";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
